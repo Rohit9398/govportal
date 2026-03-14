2281bde6308951694d5dce4d86b7e6bf781a9798
Rohit Yadav <8256rohit@gmail.com>
2026-03-15 00:25:15 +0530

    fix merge markers

--- models/user.js
+++ models/user.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
 
-name:String,
-email:String,
-password:String
-
-})
-
-=======
-const mongoose = require("mongoose")
-
-const userSchema = new mongoose.Schema({
-
-name:String,
-email:String,
-password:String
-
-})
-
->>>>>>> d5e34b406f5009b7deb13789129db66613892f74
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema);
